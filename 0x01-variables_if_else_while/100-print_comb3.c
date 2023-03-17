74821f838bc5978a12db05a6873d76f5a166f130
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-03-17 15:38:09 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+/**
+ * File: 100-print_comb3.c.
+ * Author: Mohamed K.Hegazy.
+ *
+ **/
+
+#include <stdio.h>
+
+/*
+ * main - prints all possible different combinations of two digits.
+ * Return: Always 0.
+ **/
+
+int main(void)
+{
+	int NumComb1, NumComb2;
+
+	for (NumComb1 = 0; NumComb1 < 9; NumComb1++)
+	{
+		for (NumComb2; = NumComb1 + 1; NumComb2; < 10; NumComb2++)
+		{
+			putchar((NumComb1 % 10) + '0');
+			putchar((NumComb2 % 10) + '0');
+
+			if (NumComb1 == 8 && NumComb2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
