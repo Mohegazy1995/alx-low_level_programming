d4c865be0f0cde9b05ff83e17e5fecb0cf6ca1e9
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-03-17 14:06:04 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,9 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/*
+ *this code for state the weather of number is positive or negative
+ * */
 int main(void)
 {
 	int n;
@@ -12,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	else if (n < 0)
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
