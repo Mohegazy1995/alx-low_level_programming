c1b21c638ab2fc5299daec128a9bd4a19fb48779
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-03-17 15:13:41 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+/*
+ * File: 7-print_tebahpla.c.
+ * Author: M0ohamed K.Hegazy.
+ **/
+
+#include <stdio.h>
+
+/**
+ * main-  prints the lowercase alphabet in reverse,
+ * 	You can only use the putchar function.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char rev;
+
+	for (rev = 'z', rev >= 'a'; rev--)
+	
+		putchar(rev);
+	putchar('\n');
+
+	return (0);
+
+}
