a26677ff1b51fe7fc1c2d6adbe33dabffe9fd5e8
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-03-21 14:49:37 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-putchar.c.
+ * Author: Mohamed K.Hegazy.
+ *
+ **/
+#include <stdio.h>
+
+/*
+ * Main- prints _putchar, followed by a new line.
+ * Return: Always return 0.
+ **/
+
+void putchar_Print (void);
+
+int main (void)
+{
+	putchar_Print();
+
+	return (0);
+
+}
+
+void putchar_Print (void)
+{
+	printf("_putchar\n");
+}
