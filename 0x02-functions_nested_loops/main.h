8eded4c152e0c51d5c7592965b14685ccc2722c9
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-03-21 15:50:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/**
+ * putchar_Print: that function for print _putchar
+ *      on screen followed by a new line.
+ *
+ * Return: nothing void.
+ *
+ */
+
 void putchar_Print (void);
 
 #endif
