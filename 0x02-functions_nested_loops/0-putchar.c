2bcce02cd7a18fad20a4340d017bbe376dfac7d4
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-03-21 15:45:06 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -16,7 +16,7 @@ int main(void)
 }
 
 /**
- * _putchar: that function for print _putchar
+ * putchar_Print: that function for print _putchar
  *	on screen followed by a new line.
  *
  * Return: nothing void.
