8fced0de484616763139bd411fe7f865d1b580c3
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-03-29 13:28:04 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,17 +12,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
-
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-
 	j = 0;
-
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -32,5 +30,6 @@ char *_strncat(char *dest, char *src, int n)
 
 	dest[i] = '\0';
 	return (dest);
+
 
 }
