fa0cd04aa6376d3377db5075d12a12eba91318d3
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-03-29 14:33:28 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	i = 0;
 
-	while ((i < n) && (src[i] != '\0'))
+	while((i < n) && (src[i] != '\0'))
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
-	while (i < n)
+	while(i < n)
 	{
 		dest[i] = '\0';
 		j++;
