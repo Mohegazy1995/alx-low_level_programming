c36673c0ad4b4eab4ad1661df22eec1ae0dd8657
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-04-17 15:19:15 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * MAIN -  prints the name of the file it was compiled from, followed by a new line.
+ * Return: exit code.
+ */ 
 int main(void)
 {
 	printf("%s\n", __FILE__);
