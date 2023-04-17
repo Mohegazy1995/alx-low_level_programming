bddbb64be362943e2c16d46772ffe5db8f30c6fd
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-04-17 15:27:16 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
-#ifndef FUNCTION_LIKE_MACRO_H_
-#ifdef  FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x)	((x) < (0)? -(x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
+
