5f98454ad32befa6035aa48c8cd1c3744597ebcc
Mohamed Hegazy <mohammedkhaledhegazy@gmail.com>
2023-04-28 15:31:49 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 		return (new);
 	}
-	while(temp->next)
+	while (temp->next)
 		temp = temp->next;
 	temp->next = new;
 	return (new);
